909cf1af1adc782e98af1561c67ecdcb11a80964
Nazire <nazirej@gmail.com>
2022-09-07 14:12:59 -0400

    commit tree 6dcbde1082b214389af7024fc1067f418ccdeda1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(fun="roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+// let wrapAdjective = function( start="you are") {
+//      let highlight = "*"
+//      let spec = "a hard worker"
+//     return `${start}${highlight}${spec}${highlight}`
+// }
+
+let wrapAdjective = function(highlight="*") {
+    return function(spec="special") {
+      return `You are ${highlight}${spec}${highlight}!`
+    }
+  }
